07775c63edb9b39cdb5095eae870a3bc77649313
cytgameboy <apple@appletekiMacBook-Pro.local>
2013-09-22 19:15:09 +0800

    Initial Commit

---
+++ power/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  power
+//
+//  Created by apple on 13-9-22.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
